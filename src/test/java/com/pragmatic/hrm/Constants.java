9549c4973c08b41f5acb954979cc0f5ea1008b80
pragmatictesters <janesh@pragmatictesters.com>
2020-11-19 22:50:45 +0530

    Added Constants.java

---
+++ src/test/java/com/pragmatic/hrm/Constants.java
@@ -0,0 +1,15 @@
+package com.pragmatic.hrm;
+
+/**
+ * Created by Pragmatic Test Labs (Private) Limited
+ *
+ * @Author Janesh Kodikara
+ */
+public class Constants {
+
+    public static  String ADMIN_PASSWORD  ;
+    public static  String ADMIN_USERNAME ;
+    public static String BROWSER_TYPE ;
+    public static String BASE_URL;
+
+}
